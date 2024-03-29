b7c6f53bf9a2dccc3b4b3b47992383a39e690d4c
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-03-29 23:21:48 +1000

    330. Working with Navigation Links (NavLink)

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -1,4 +1,4 @@
-﻿import { Link } from "react-router-dom";
+﻿import { NavLink } from "react-router-dom";
 import cssClasses from "./MainNavigation.module.css";
 export default function MainNavigation() {
   return (
@@ -7,10 +7,30 @@ export default function MainNavigation() {
         <nav>
           <ul className={cssClasses.list}>
             <li>
-              <Link to="/">Home</Link>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? cssClasses.active : undefined
+                }
+                end //this is to make sure that the link is only active when the path is exactly the same as the link
+
+                // style={({ isActive }) => ({
+                //   textDecoration: isActive ? "underline" : undefined,
+                //   color: isActive ? "red" : undefined,
+                // })}
+              >
+                Home
+              </NavLink>
             </li>
             <li>
-              <Link to="/products">Products</Link>
+              <NavLink
+                to="/products"
+                className={({ isActive }) =>
+                  isActive ? cssClasses.active : undefined
+                }
+              >
+                Products
+              </NavLink>
             </li>
           </ul>
         </nav>
