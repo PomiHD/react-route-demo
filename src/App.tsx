436c29308744aa5b112b2bdd3a79654c60c19cec
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-03-29 22:21:13 +1000

    327. Navigating between Pages with Links

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,18 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Products from "./pages/Products.tsx";
 
 // define the router
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Home />,
-//   },
-//   {
-//     path: "/products",
-//     element: <Products />,
-//   },
-// ]);
-
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<Home />} />,
-    <Route path="/products" element={<Products />} />,
-  </Route>,
-);
-
-const router = createBrowserRouter(routeDefinitions);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+]);
 function App() {
   return (
     <>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
-﻿export default function Home() {
-    return (
-        <><div>
-            My Home Page
-        </div> </>
-    );
+﻿import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        Go to the <Link to="/products">Products</Link> page.
+      </p>
+    </>
+  );
 }
