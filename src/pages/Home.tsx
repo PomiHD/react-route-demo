27ce8f8513f969e259ad14460b836b83127714c4
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-03-29 23:30:58 +1000

    331. Navigating Programmatically

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,20 @@
-﻿import { Link } from "react-router-dom";
+﻿import { Link, useNavigate } from "react-router-dom";
 
 export default function Home() {
+  //why useNavigate() instead of <Link>?
+  //useNavigate() is used to navigate programmatically, while <Link> is used to navigate through the UI.
+  //for example, when you want to navigate after a form submission or after a button click.
+  const navigate = useNavigate();
+  function navigateHandler() {
+    navigate("/products");
+  }
   return (
     <>
       <h1>My Home Page</h1>
       <p>
         Go to the <Link to="/products">Products</Link> page.
       </p>
+      <button onClick={navigateHandler}>Navigate</button>
     </>
   );
 }
