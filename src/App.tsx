308dd0454a63d8a7ab0527b3e4b0e7b11f8bd29e
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-03-29 22:13:18 +1000

    another way to define the route

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Products from "./pages/Products.tsx";
 
 // define the router
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/products",
-    element: <Products />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home />,
+//   },
+//   {
+//     path: "/products",
+//     element: <Products />,
+//   },
+// ]);
+
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<Home />} />,
+    <Route path="/products" element={<Products />} />,
+  </Route>,
+);
+
+const router = createBrowserRouter(routeDefinitions);
 function App() {
   return (
     <>
